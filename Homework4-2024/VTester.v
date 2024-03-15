a172daec3cfff1680521cf993ce4eb78d985c765
Evan <evan.apinis@gmail.com>
2024-03-15 15:12:50 -0400

    Writing report

--- Homework4-2024/VTester.v
+++ Homework4-2024/VTester.v
@@ -12,23 +12,24 @@ module VTester ();
 	reg clk = 1'b 0;
 	reg [1:ff] pre_expected_st, cur_expected_st, scanIn, saved_st;
 	wire So;
-	wire [0:outWidth - 1] PO;
+	wire [outWidth - 1:0] PO;
 	reg [outWidth - 1:0] expected_PO, SampledPO;
 	reg [outWidth + inWidth + (2 * ff):1] line;
 	
 	reg [8*50:1] wireName; 
-    reg stuckAtVal; 
+  reg stuckAtVal; 
 	
-	integer testFile, faultFile, status, index, numOfDetected;
+	integer testFile, faultFile, status, index, numOfDetected, numOfFaults;
 	reg detected = 1'b 0;
 	reg flag = 1'b 0;
 
-	SSC_net FUT (.clk(clk), .rst(1'b 0), .start(PI[1]), .read_reg(PO[24]), .write_reg(PO[25]), .readdata(PI[2:17]), .writedata(PO[0:15]), .addr(PO[16:23]), .done(PO[26]), .Si(Si), .So(So), .NbarT(NbarT));  
-    always #30 clk = ~clk; 
+	SSC_net FUT (.clk(clk), .rst(1'b 0), .start(PI[1]), .read_reg(PO[24]), .write_reg(PO[25]), .readdata(PI[2:17]), .writedata(PO[15:0]), .addr(PO[23:16]), .done(PO[26]), .Si(Si), .So(So), .NbarT(NbarT));  
+  
+  always #30 clk = ~clk; 
 
 	initial begin
-	 // $display($time);
 		numOfDetected = 0;
+    numOfFaults = 0;
 		faultFile = $fopen ("SSC.flt", "w");
 		$FaultCollapsing(VTester.FUT,"SSC.flt");
 		$fclose(faultFile);
@@ -36,18 +37,15 @@ module VTester ();
 		
 		faultFile = $fopen ("SSC.flt", "r");
 		
-        while( !$feof(faultFile)) 
-		begin 
+    while( !$feof(faultFile)) begin 
 			status = $fscanf(faultFile, "%s s@%b\n", wireName, stuckAtVal); 
 			detected = 1'b 0;
-			testFile = $fopen ("SSC.pat", "r");
+			testFile = $fopen ("SSC.tst", "r");
 			flag = 1'b 0;
 			$InjectFault(wireName, stuckAtVal);
 			#2
-			while((!$feof(testFile))&&(!detected))
-			begin 
+			while((!$feof(testFile))&&(!detected)) begin 
 				pre_expected_st = cur_expected_st;  
-				//status = $fscanf(testFile,"%b\n",PI, scanIn, cur_expected_st, expected_PO);
 				status = $fscanf(testFile,"%b\n", line);
 				PI = line[outWidth + inWidth + (2 * ff):outWidth + 1 + (2 * ff)];
 				scanIn = line[outWidth + (2 * ff):outWidth + ff + 1];
@@ -69,37 +67,28 @@ module VTester ();
 				NbarT = 1'b0;  
 				@(posedge clk);  
 					SampledPO = PO;  
-				if (flag == 1'b 0)
-				begin
+				if (flag == 1'b 0) begin
 					flag = 1'b 1;
-					if(expected_PO != SampledPO)  
-					begin 
-						//numOfDetected = numOfDetected + 1;  
+					if(expected_PO != SampledPO) begin  
 						detected = 1; 
 						$display ("missmatch ", $time);
-				//		$stop;
 					end
 					else
 					$display ("OKto1");	
 				end
-				else
-				begin
-					if({pre_expected_st, expected_PO} != {saved_st, SampledPO})  
-					begin 
-						//numOfDetected = numOfDetected + 1;  
+				else begin
+					if({pre_expected_st, expected_PO} != {saved_st, SampledPO}) begin 
 						detected = 1;  
 						$display ("missmatch ", $time);
-			//			$stop;
 					end 
 					else
 						$display ("OKto2");
 				end
 				#5;  
 
-            end//test 
+      end//test 
 			
-			if (detected != 1)
-			begin
+			if (detected != 1) begin
 				NbarT = 1;
 				index = 1;
 				repeat(ff) begin 
@@ -113,17 +102,18 @@ module VTester ();
 					detected = 1;
 			end
 			
-			$fclose (testFile);
 			if (detected == 1)
 				numOfDetected = numOfDetected + 1;
-			$RemoveFault(wireName); 
-			#3;
-				
-         
-        end//fault 
-		$display ("detected faults: ", numOfDetected);
-		$display($time);
-		$stop;
-		
-    end//initial 
+			$RemoveFault(wireName);
+      numOfFaults = numOfFaults + 1;
+      $fclose (testFile);
+
+    end//fault 
+  $fclose(faultFile);
+  $display ("Number of faults: ", numOfFaults);
+  $display ("Number of detected faults: ", numOfDetected);
+  $display ("Coverage: ", numOfDetected * 100.0 / numOfFaults);
+  $stop;
+  
+  end//initial 
 endmodule 
